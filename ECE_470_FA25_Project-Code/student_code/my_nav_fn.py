87fa844fe5602b11f88f9e5be5715349a575286e
masgras2 <masgras2@illinois.edu>
2025-12-10 16:23:56 -0600

    Add commented out robot collision detection

--- ECE_470_FA25_Project-Code/student_code/my_nav_fn.py
+++ ECE_470_FA25_Project-Code/student_code/my_nav_fn.py
@@ -31,7 +31,8 @@ def compute_gradients(state, targets, obstacles, r):
     ## Navigation field ##
 
     # obs_detect_rad = 0.25
-    k = 5
+    k = 0.8
+    k_bot = 3
     K_weight = 1
     safetyZone = 0.95
     boundaryStrength = 1
@@ -72,7 +73,7 @@ def compute_gradients(state, targets, obstacles, r):
           if np.dot(Fgoal[i]/np.linalg.norm(Fgoal[i]), d_obs[i]/np.linalg.norm(d_obs[i])) < -0.99:
             d_obs[i][0] = -Fgoal[i][0] - np.sin(angle)
             d_obs[i][1] = -Fgoal[i][1] + np.cos(angle)
-            # print(f"Colinearity detected! applying force ({d_obs[i][0]}, {d_obs[i][1]})")
+          # Default repulsion function
           else:
             magnitude = k/np.linalg.norm(d_obs[i])**3
             d_obs[i][0] = np.cos(angle)*magnitude
@@ -89,14 +90,46 @@ def compute_gradients(state, targets, obstacles, r):
       #  print(obs_forces[i + 1])
        ob_F_total = np.add(ob_F_total, obs_forces[i + 1])
        
-    #dist_obs = obstacles - state
-    # Fobs = k/dist_obs**3 *dd/dq 
+    # rob_forces = []
+    # for j in range(len(state)):
+    #   d_rob = np.copy(state) #get size of state vector
+    #   for i in range(len(d_rob)):
+    #      d_rob[i][0] = state[i][0] - state[j][0]
+    #      d_rob[i][1] = state[i][1] - state[j][1]
 
+    #      angle = np.arctan2(d_obs[i][1], d_obs[i][0])
 
-    ####collision repulsion  (1 potential)
+    #      d_rob[i][0] -= np.cos(angle)*r
+    #      d_rob[i][1] -= np.sin(angle)*r
+
+    #   for i in range(len(d_rob)):
+    #     # Obstacle detected and close enough to move
+    #     if (i < j and np.linalg.norm(d_rob[i]) < 2.1*r) or (j < i and np.linalg.norm(d_rob[i]) < 3*r):
+    #       # Colinearity detection
+    #       angle = np.arctan2(d_rob[i][1], d_rob[i][0])
+    #       if np.dot(Fgoal[i]/np.linalg.norm(Fgoal[i]), d_rob[i]/np.linalg.norm(d_rob[i])) < -0.99:
+    #         d_rob[i][0] = -Fgoal[i][0] - np.sin(angle)
+    #         d_rob[i][1] = -Fgoal[i][1] + np.cos(angle)
+    #       # Default repulsion function
+    #       else:
+    #         magnitude = k_bot/np.linalg.norm(d_rob[i])**3
+    #         d_rob[i][0] = np.cos(angle)*magnitude
+    #         d_rob[i][1] = np.sin(angle)*magnitude
+    #     else:
+    #       d_rob[i][0] = 0 
+    #       d_rob[i][1] = 0
+      
+    #   rob_forces.append(d_rob)
+
+    # rob_F_total = np.copy(rob_forces[0])
+    # for i in range(len(rob_forces) - 1):
+    #   #  print(ob_F_total)
+    #   #  print(obs_forces[i + 1])
+    #    rob_F_total = np.add(rob_F_total, rob_forces[i + 1])
 
     # return np.add(Fboundary, np.add(Fgoal, ob_F_total))
     Fsum = np.add(Fgoal, ob_F_total)
+    # Fsum = np.add(Fsum, rob_F_total)
 
     # for i in range(len(Fsum)):
     #    if np.linalg.norm(Fsum[i]) < 0.001:
